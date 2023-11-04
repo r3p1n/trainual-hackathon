260ff0198ae0cd517a185842ae603d40a667b292
Dmytro Repin <dmytrorepin@gmail.com>
2023-11-04 16:57:34 +0000

    background

--- app/layout.js
+++ app/layout.js
@@ -11,7 +11,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header></header>
+        <div>
+          <nav></nav>
+          <main></main>
+        </div>
+        {children}
+      </body>
+      
     </html>
   )
 }
